ebd91c2470be15465b6eef6a98b7cae1c290db95
Mario Fernando Araya Royo <arayaroyo686@gmail.com>
2024-12-02 14:29:23 -0600

    add migrations

---
+++ migrations/safetrust/1733160060158_escrow_xdr_transactions/down.sql
@@ -0,0 +1,5 @@
+-- Eliminar tabla escrow_xdr_transactions
+DROP TABLE IF EXISTS escrow_xdr_transactions;
+
+-- Eliminar tipo ENUM xdr_status
+DROP TYPE IF EXISTS xdr_status;
---
+++ migrations/safetrust/1733171122098_escrow_api_calls/down.sql
@@ -0,0 +1,2 @@
+-- Eliminar tabla escrow_api_calls
+DROP TABLE IF EXISTS escrow_api_calls;
