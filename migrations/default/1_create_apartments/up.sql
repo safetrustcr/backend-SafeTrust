aa484d0537741e6c7e286a701bc3711e60e42df2
Nathan_akin <85641756+akintewe@users.noreply.github.com>
2025-01-23 22:58:48 +0100

    feat(migrations): add test data for apartment_images

--- migrations/default/1_create_apartments/up.sql
+++ migrations/default/1_create_apartments/up.sql
@@ -7,6 +7,6 @@ CREATE TABLE apartments (
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
--- Insert a test apartment
+
 INSERT INTO apartments (id, name, description) VALUES 
 ('11111111-1111-1111-1111-111111111111', 'Modern Loft in Heredia', 'A beautiful modern loft'); 
