9d1af0c94f9af8a7791a19dda676bcd8b8869837
GideonBature <infoaboutgideon@gmail.com>
2025-01-24 04:04:12 +0100

    Perf: perf(users table): add remove query for index and unique contraint

--- migrations/safetrust/1731908676359_create_users/down.sql
+++ migrations/safetrust/1731908676359_create_users/down.sql
@@ -1,1 +1,8 @@
+-- Remove indices
+DROP INDEX IF EXISTS idx_users_email;
+
+-- Remove UNIQUE constraint
+ALTER TABLE users
+    DROP CONSTRAINT IF EXISTS users_email_unique;
+
 DROP TABLE IF EXISTS users;
