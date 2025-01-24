c7ebcf16954f745e723c70554dee298897571854
GideonBature <infoaboutgideon@gmail.com>
2025-01-24 04:02:41 +0100

    Perf: perf(users table): add unique contraint and indices for query

--- migrations/safetrust/1731908676359_create_users/up.sql
+++ migrations/safetrust/1731908676359_create_users/up.sql
@@ -3,3 +3,10 @@ CREATE TABLE users (
     email TEXT NOT NULL,
     last_seen TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
+
+-- Add UNIQUE constraint to email
+ALTER TABLE users 
+    ADD CONSTRAINT users_email_unique UNIQUE (email);
+
+-- Add indices for common queries
+CREATE INDEX idx_users_email ON users(email);
