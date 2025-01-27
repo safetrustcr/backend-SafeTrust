6e1e931969096fbcd5a510cc5551f73a514f9d32
Nathan_akin <85641756+akintewe@users.noreply.github.com>
2025-01-27 22:44:42 +0100

    made changes

--- migrations/safetrust/1734473134981_test_data/up.sql
+++ migrations/safetrust/1734473134981_test_data/up.sql
@@ -1,2 +1,5 @@
+INSERT INTO apartments (id, name, description) VALUES 
+('11111111-1111-1111-1111-111111111111', 'Modern Loft in Heredia', 'A beautiful modern loft');
+
 INSERT INTO apartment_images (id, apartment_id, image_url) VALUES 
 ('11111111-1111-1111-1111-111111111111', '11111111-1111-1111-1111-111111111111', 'https://example.com/test-image.jpg'); 
