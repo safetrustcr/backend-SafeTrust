23375f5392049410939a654b8ef16855a0808ec5
villarley <santivillarley1010@gmail.com>
2025-03-03 23:35:34 -0600

    feat: moved trigger to webhook direction

--- webhook/index.js
+++ webhook/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-const webhooksRoutes = require('./routes/webhooks');
+const webhooksRoutes = require('./webhooks');
 
 const app = express();
 
