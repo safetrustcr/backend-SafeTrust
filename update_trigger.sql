7c8e4551624d6b23eff94c25307d8734893b3378
akintewe <85641756+akintewe@users.noreply.github.com>
2025-03-28 11:07:00 +0100

    made fixes

---
+++ update_trigger.sql
@@ -0,0 +1,1 @@
+CREATE OR REPLACE FUNCTION public.update_updated_at_column() RETURNS TRIGGER AS $$ BEGIN NEW.updated_at = NOW(); RETURN NEW; END; $$ LANGUAGE plpgsql; CREATE TRIGGER update_updated_at BEFORE UPDATE ON public.hotels FOR EACH ROW EXECUTE FUNCTION public.update_updated_at_column();
