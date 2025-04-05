b294f2e36bf54ac36d78e5533446673673dc8c13
Ekuma Mathew <ekumamatthew1@gmail.com>
2025-04-05 03:13:54 +0100

    fix: rm trigger drops

--- migrations/hotel_industry/1743157433016_create_users_table/down.sql
+++ migrations/hotel_industry/1743157433016_create_users_table/down.sql
@@ -1,8 +1,3 @@
--- Drop trigger first
-DROP TRIGGER IF EXISTS update_users_updated_at ON users;
-
--- Drop trigger function
-DROP FUNCTION IF EXISTS update_updated_at_column();
 
 -- Drop indexes
 DROP INDEX IF EXISTS idx_users_email;
