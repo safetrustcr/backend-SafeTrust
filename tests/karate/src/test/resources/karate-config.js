a1cfaf7ef276d08c4cfb9dbad8aab611752f34d4
emarc99 <57766083+emarc99@users.noreply.github.com>
2026-04-28 05:06:17 +0100

    chore: config karate environment

--- tests/karate/src/test/resources/karate-config.js
+++ tests/karate/src/test/resources/karate-config.js
@@ -8,8 +8,51 @@ function fn() {
   // Basic configuration
   var config = {
     baseUrl: "http://graphql-engine-test:8080/v1/graphql",
-    webhookUrl: "http://localhost:3001",
+    webhookUrl: karate.properties['webhookUrl'] || "http://webhook:3001",
     adminSecret: "myadminsecretkey",
+  };
+
+  // DB helper
+  config.db = {
+    query: function(sql) {
+      if (typeof sql !== 'string') sql = new java.lang.String(sql).toString();
+      var DriverManager = Java.type('java.sql.DriverManager');
+      var conn = DriverManager.getConnection(
+        'jdbc:postgresql://postgres_test:5432/postgres',
+        'postgres',
+        'postgrespassword'
+      );
+      var stmt = conn.createStatement();
+      var rs = stmt.executeQuery(sql);
+      var meta = rs.getMetaData();
+      var cols = meta.getColumnCount();
+      var list = [];
+      while (rs.next()) {
+        var map = {};
+        for (var i = 1; i <= cols; i++) {
+          var name = meta.getColumnName(i);
+          map[name] = rs.getString(i);
+        }
+        list.push(map);
+      }
+      rs.close();
+      stmt.close();
+      conn.close();
+      return list;
+    },
+    execute: function(sql) {
+      if (typeof sql !== 'string') sql = new java.lang.String(sql).toString();
+      var DriverManager = Java.type('java.sql.DriverManager');
+      var conn = DriverManager.getConnection(
+        'jdbc:postgresql://postgres_test:5432/postgres',
+        'postgres',
+        'postgrespassword'
+      );
+      var stmt = conn.createStatement();
+      stmt.execute(sql);
+      stmt.close();
+      conn.close();
+    }
   };
 
   // Token helper function
