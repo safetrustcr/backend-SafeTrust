a3035a7159edc82e74c421beb6e224d81eae95a0
emarc99 <57766083+emarc99@users.noreply.github.com>
2026-06-02 08:44:17 +0100

    fix: added the 6 missing demo users

--- seeds/safetrust/1733963959819_user_seed.sql
+++ seeds/safetrust/1733963959819_user_seed.sql
@@ -35,5 +35,71 @@ VALUES
         '+506',
         'Escazú, Costa Rica',
         NOW()
+    ),
+    (
+        'demo-user-uid-003',
+        'demo-user-uid-003',
+        'maria.rodriguez@example.com',
+        'María',
+        'Rodríguez',
+        '88005566',
+        '+506',
+        'San José, Costa Rica',
+        NOW()
+    ),
+    (
+        'demo-user-uid-004',
+        'demo-user-uid-004',
+        'ana.castro@example.com',
+        'Ana',
+        'Castro',
+        '88007788',
+        '+506',
+        'San José, Costa Rica',
+        NOW()
+    ),
+    (
+        'demo-user-uid-005',
+        'demo-user-uid-005',
+        'pedro.solano@example.com',
+        'Pedro',
+        'Solano',
+        '88009900',
+        '+506',
+        'San José, Costa Rica',
+        NOW()
+    ),
+    (
+        'demo-user-uid-006',
+        'demo-user-uid-006',
+        'carlos.jimenez@example.com',
+        'Carlos',
+        'Jiménez',
+        '88001133',
+        '+506',
+        'San José, Costa Rica',
+        NOW()
+    ),
+    (
+        'demo-user-uid-007',
+        'demo-user-uid-007',
+        'roberto.mora@example.com',
+        'Roberto',
+        'Mora',
+        '88002244',
+        '+506',
+        'San José, Costa Rica',
+        NOW()
+    ),
+    (
+        'demo-user-uid-008',
+        'demo-user-uid-008',
+        'laura.vargas@example.com',
+        'Laura',
+        'Vargas',
+        '88003355',
+        '+506',
+        'San José, Costa Rica',
+        NOW()
     )
 ON CONFLICT (id) DO NOTHING;
