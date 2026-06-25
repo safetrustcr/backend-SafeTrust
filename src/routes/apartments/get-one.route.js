e05201b3928862508e7738aa2f2e1c3064eb574e
Josue Soto <sotoherrerajosue@gmail.com>
2026-06-25 13:12:50 -0600

    chore: remove src/routes dead folder, not used by the webhook service

--- src/routes/apartments/get-one.route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { requireAuth } = require('../../middleware/auth');
-const { getApartmentHandler } = require('./get-one.handler');
-
-const router = express.Router();
-
-router.get('/:id', requireAuth, getApartmentHandler);
-
-module.exports = router;
